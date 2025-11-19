bbf5342c4b6f16d70a69ce4f9babd99831014973
Viraj Shrivastav <virajshrivastav919@gmail.com>
2025-11-19 16:52:57 +0530

    fix: Correct Sonner import and add missing font assets

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
-import { Sonner } from "@/components/ui/sonner";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import App from "./App";
 import "@/index.css";
 
